82412cc45d40fd51c36429e648e60a70e416e906
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2025-04-23 15:29:46 -0300

    feat: validating login form

--- src/app/components/login-tab/login-tab.component.ts
+++ src/app/components/login-tab/login-tab.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms'
+import { FormType } from "./login-tab.types"
 
 @Component({
   selector: 'app-login-tab',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-tab.component.html'
 })
+
 export class LoginTabComponent {
 
+  loginForm = new FormGroup({
+    username: new FormControl('', [
+      Validators.minLength(4),
+      Validators.required
+    ]),
+    password: new FormControl('', [
+      Validators.minLength(5),
+      Validators.required
+    ])
+  })
+
+  validateForm = (form: FormType): boolean => form.valid
+
+  handleLogin = () => {
+    
+    if(!this.validateForm(this.loginForm))
+      return
+
+    console.log(this.loginForm.valid)
+  }
 }
