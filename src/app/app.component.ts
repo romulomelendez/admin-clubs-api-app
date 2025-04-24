b20c5f093fbd6abda571072da4ba6bf32c8b2e31
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2025-04-24 18:47:14 -0300

    feat: main route created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { HomeComponent } from "./pages/home/home.component";
+import { CommonModule } from '@angular/common'
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
-    HomeComponent
+    RouterOutlet
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
-  title = 'admin-clubs-api-app';
+  title = 'admin-clubs-api-app'
 }
